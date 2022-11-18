c7fe81955d1794a368015b1b6d69514c90f7e633
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-11-18 20:34:18 +0100

    Format date

--- src/components/top-debts/Debt.tsx
+++ src/components/top-debts/Debt.tsx
@@ -4,11 +4,20 @@ type Props = {
   debt: DebtWithoutId;
 };
 
+const padTo2Digits = (num: number) => num.toString().padStart(2, "0");
+
+const formatDate = (date: Date) =>
+  [
+    padTo2Digits(date.getDate()),
+    padTo2Digits(date.getMonth() + 1),
+    date.getFullYear(),
+  ].join("-");
+
 const Debt = (props: Props): JSX.Element => {
   const { Name, NIP, Value, Date: date } = props.debt;
 
   const newDate = new Date(date);
-  const formattedDate = newDate.toLocaleDateString();
+  const formattedDate = formatDate(newDate);
 
   return (
     <tr>
